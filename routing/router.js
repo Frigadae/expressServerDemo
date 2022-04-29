c31a535c6c6aa4a9632193c34d7eb22117045758
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2022-04-29 17:12:06 +1200

    logger now writes to a log file

--- routing/router.js
+++ routing/router.js
@@ -3,13 +3,20 @@ const router = express.Router();
 const logger = require("../utils/logger.js");
 
 router.use("*", (req, res, next) => {
-    logger.print(req);
+    logger.print(req, res);
     next()
 });
 
-router.all("*", (req, res) => {
-    let message = {"message": "Acknowledged"};
+router.get("/", (req, res) => {
+    let message = {"message": "welcome to the landing page"};
     res.status(200)
+    res.type("json");
+    res.send(message);
+})
+
+router.all("*", (req, res) => {
+    let message = {"message": "invalid path"};
+    res.status(400)
     res.type("json");
     res.send(message);
 });
--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,26 @@
-function print(req) {
-    console.log("-+-+-+-+-+-"); 
-    console.log(`Request made at ${new Date()}`);
-    console.log(req.params); 
-    console.log(req.query); 
-    console.log("-+-+-+-+-+-"); 
+const fs = require("fs");
+const logFile = "requestLog.log";
+
+function print(req, res) {
+    let reqInfo = {
+        "timestamp": new Date(),
+        "method": req.method,
+        "hostname": req.hostname,
+        "parameters": req.params,
+        "queries": req.query
+    };
+    
+    res.on("finish", () => {
+        console.log(`Response status: ${res.statusCode}`);
+    }) 
+    try {
+        fs.appendFile(logFile, JSON.stringify(reqInfo) + "\r\n", () => {
+            console.log(`Request saved to ${logFile}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 exports.print = print;
