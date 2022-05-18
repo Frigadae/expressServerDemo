0cee97d9c59c783c5ff08722288ad19ef94a9a33
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2022-05-18 13:45:50 +1200

    log status code of res in req object when logging

--- utils/logger.js
+++ utils/logger.js
@@ -3,20 +3,14 @@ const logFile = "requestLog.log";
 
 function print(req, res) {
     let reqInfo = {
-        "type": "request",
         "timestamp": new Date(),
         "method": req.method,
         "hostname": req.hostname,
         "parameters": req.params,
         "queries": req.query,
         "body": req.body,
+        "responseCode": res.statusCode
     };
-
-    let resInfo = {
-        "type": "response",
-        "timestamp": new Date(),
-        "status": res.statusCode,
-    }
     
     res.on("finish", () => {
         console.log(`Request info: ${JSON.stringify(reqInfo)}`);
@@ -24,9 +18,6 @@ function print(req, res) {
         try {
             fs.appendFile(logFile, JSON.stringify(reqInfo) + "\r\n", () => {
                 console.log(`Request saved to ${logFile}`);
-            });
-            fs.appendFile(logFile, JSON.stringify(resInfo) + "\r\n", () => {
-                console.log(`Response saved to ${logFile}`);
             });
         } catch (error) {
             console.log(error);
